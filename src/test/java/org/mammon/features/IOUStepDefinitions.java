760dbcbd939736f73492e3267b86e59ff978036a
Daan van Berkel <daan.v.berkel.1980@gmail.com>
2012-03-26 08:48:31 +0200

    User Story U2
    The user story U2 is implemented.

--- src/test/java/org/mammon/features/IOUStepDefinitions.java
+++ src/test/java/org/mammon/features/IOUStepDefinitions.java
@@ -1,28 +1,43 @@
 package org.mammon.features;
 
+import static junit.framework.Assert.fail;
 import cucumber.annotation.en.Given;
 import cucumber.annotation.en.Then;
 import cucumber.annotation.en.When;
-import static junit.framework.Assert.fail;
 
 public class IOUStepDefinitions {
 	@Given("^(\\w+) owes (\\w+) (\\d+\\.\\d{2}) euros")
-	public void aOwesBMoney(String personA, String personB, String amount) {
+	public void givenBorrowerOwesLenderMoney(String personA, String personB, String amount) {
 
 	}
 
 	@When("^(\\w+) creates an IOU of (\\d+\\.\\d{2}) euros")
-	public void aCreatesAnIOU(String person, String amount) {
+	public void whenBorrowerCreatesAnIOU(String person, String amount) {
 
 	}
 
 	@When("^sends the IOU to (\\w+)")
-	public void sendsTheIOUTo(String person) {
+	public void whenBorrowerSendsTheIOULender(String person) {
 
 	}
 
 	@Then("^(\\w+) should hold an IOU of (\\d+\\.\\d{2}) euros redeemable by (\\w+)")
-	public void bHoldsIOU(String personB, String amount, String personA) {
+	public void thenLenderHoldsIOU(String personB, String amount, String personA) {
+		fail();
+	}
+
+	@Given("^(\\w+) holds an IOU of (\\d+\\.\\d{2}) euros redeemable by (\\w+)")
+	public void givenLenderHoldsIOU(String lender, String amount, String borrower) {
+
+	}
+
+	@When("(\\w+) lists the IOU's")
+	public void whenLenderListsIOUs(String lender) {
+
+	}
+
+	@Then("(\\w+) should have the IOU \\[(\\w+), (\\d+\\.\\d{2})\\]")
+	public void thenLenderShouldOwnIOU(String lender, String borrower, String amount) {
 		fail();
 	}
 }
--- src/test/resources/org/mammon/features/IOU.feature
+++ src/test/resources/org/mammon/features/IOU.feature
@@ -8,3 +8,12 @@ Feature: IOU
     When Bob creates an IOU of 12.00 euros
     And sends the IOU to Alice
     Then Alice should hold an IOU of 12.00 euros redeemable by Bob
+    
+  Scenario: List IOUs
+    Given Alice holds an IOU of 1.00 euros redeemable by Bob
+    And Alice holds an IOU of 2.00 euros redeemable by Carol
+    And Alice holds an IOU of 3.00 euros redeemable by Dave
+    When Alice lists the IOU's
+    Then Alice should have the IOU [Bob, 1.00]
+    And Alice should have the IOU [Coral, 2.00]
+    And Alice should have the IOU [Dave, 3.00]
