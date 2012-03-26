ad3229bb9eb834b28157ff2765027fd588a1c163
Daan van Berkel <daan.v.berkel.1980@gmail.com>
2012-03-26 08:58:08 +0200

    [REFACTOR] Regex tightened
    The regular expression are tightened to precisely match the entiry
    line.

--- src/test/java/org/mammon/features/IOUStepDefinitions.java
+++ src/test/java/org/mammon/features/IOUStepDefinitions.java
@@ -6,37 +6,37 @@ import cucumber.annotation.en.Then;
 import cucumber.annotation.en.When;
 
 public class IOUStepDefinitions {
-	@Given("^(\\w+) owes (\\w+) (\\d+\\.\\d{2}) euros")
+	@Given("^(\\w+) owes (\\w+) (\\d+\\.\\d{2}) euros$")
 	public void givenBorrowerOwesLenderMoney(String personA, String personB, String amount) {
 
 	}
 
-	@When("^(\\w+) creates an IOU of (\\d+\\.\\d{2}) euros")
+	@When("^(\\w+) creates an IOU of (\\d+\\.\\d{2}) euros$")
 	public void whenBorrowerCreatesAnIOU(String person, String amount) {
 
 	}
 
-	@When("^sends the IOU to (\\w+)")
+	@When("^sends the IOU to (\\w+)$")
 	public void whenBorrowerSendsTheIOULender(String person) {
 
 	}
 
-	@Then("^(\\w+) should hold an IOU of (\\d+\\.\\d{2}) euros redeemable by (\\w+)")
+	@Then("^(\\w+) should hold an IOU of (\\d+\\.\\d{2}) euros redeemable by (\\w+)$")
 	public void thenLenderHoldsIOU(String personB, String amount, String personA) {
 		fail();
 	}
 
-	@Given("^(\\w+) holds an IOU of (\\d+\\.\\d{2}) euros redeemable by (\\w+)")
+	@Given("^(\\w+) holds an IOU of (\\d+\\.\\d{2}) euros redeemable by (\\w+)$")
 	public void givenLenderHoldsIOU(String lender, String amount, String borrower) {
 
 	}
 
-	@When("(\\w+) lists the IOU's")
+	@When("^(\\w+) lists the IOU's$")
 	public void whenLenderListsIOUs(String lender) {
 
 	}
 
-	@Then("(\\w+) should have the IOU \\[(\\w+), (\\d+\\.\\d{2})\\]")
+	@Then("^(\\w+) should have the IOU \\[(\\w+), (\\d+\\.\\d{2})\\]$")
 	public void thenLenderShouldOwnIOU(String lender, String borrower, String amount) {
 		fail();
 	}
