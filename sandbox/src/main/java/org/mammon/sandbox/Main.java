acc4825ddbd43d15f0709d9d9448444ef32e61a5
Mark van Cuijk <github@mark.van-cuijk.nl>
2012-07-03 23:35:26 +0200

    Initial attempt at creating an interactive version

--- sandbox/src/main/java/org/mammon/sandbox/Main.java
+++ sandbox/src/main/java/org/mammon/sandbox/Main.java
@@ -1,8 +1,16 @@
 package org.mammon.sandbox;
 
+import java.io.BufferedReader;
 import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.HashMap;
+import java.util.Map;
+import java.util.StringTokenizer;
 import java.util.concurrent.TimeUnit;
 
+import org.mammon.Bearer;
+import org.mammon.IOweYou;
+import org.mammon.Issuer;
 import org.mammon.math.Gq;
 import org.mammon.math.Z;
 import org.mammon.messaging.Identifiable;
@@ -27,10 +35,12 @@ import org.mammon.sandbox.real.example.ExampleSignatureHashFunction;
 import org.mammon.sandbox.real.example.ExampleSpentCoin;
 import org.mammon.sandbox.real.example.ExampleUnspentCoin;
 import org.mammon.scheme.brands.accountholder.AccountHolder;
+import org.mammon.scheme.brands.bank.Bank;
 import org.mammon.scheme.brands.coin.SpentCoin;
 import org.mammon.scheme.brands.coin.UnspentCoin;
 import org.mammon.scheme.brands.generic.accountholder.AbstractOpeningAccountHolder;
 import org.mammon.scheme.brands.generic.assettypes.EuroAssetType;
+import org.mammon.scheme.brands.generic.bank.AbstractBank;
 import org.mammon.scheme.brands.generic.bank.AbstractBankPrivate;
 import org.mammon.scheme.brands.generic.coin.AbstractUnspentCoin;
 import org.mammon.scheme.brands.messages.BankWitnessesRequest;
@@ -47,11 +57,19 @@ import org.mammon.scheme.brands.messages.TransferToShopMessage;
 
 public class Main {
 
+	private ExampleSetup setup;
+	
+	private MessagingSystem messaging;
+	
+	private Map<String, Bank> banks = new HashMap<String, Bank>();
+	
 	public static void main(String[] args) throws InterruptedException, IOException {
 		if ("1".equals(args[0])) {
-			mainReal(args);
+			new Main().mainReal(args);
 		} else if ("2".equals(args[0])) {
-			mainReal2(args);
+			new Main().mainReal2(args);
+		} else if ("i".equals(args[0])) {
+			new Main().mainInteractive(args);
 		}
 	}
 
@@ -150,14 +168,8 @@ public class Main {
 	// System.exit(0);
 	//
 	// }
-
-	/**
-	 * @param args
-	 * @throws InterruptedException
-	 * @throws IOException
-	 */
-	public static void mainReal(String[] args) throws InterruptedException, IOException {
-		final String localId = "real1"; //UUID.randomUUID().toString();
+	
+	private void setupNode(String localId) throws IOException {
 		final JsonUtil jsonUtil = new JsonUtil(EuroAssetType.class, Z.class, Z.ZElement.class, Gq.class,
 				Gq.GqElement.class, ExampleSetup.class, ExampleBank.class, RemoteBank.class, BlindedIdentity.class,
 				IssuedWitnesses.class, ExampleAccountHolder.class, OpeningAccountHolder.class, ExampleShop.class,
@@ -167,14 +179,113 @@ public class Main {
 				BlindedIdentityResponse.class, IssueCoinsRequest.class, IssueCoinsResponse.class,
 				ObtainCoinsMessage.class, TransferToShopMessage.class, CoinHashRequest.class, CoinHashResponse.class,
 				CoinTransferMessage.class);
-		final MessagingSystem messaging = new MessagingSystem(jsonUtil, localId);
+		messaging = new MessagingSystem(jsonUtil, localId);
 
 		// Setup the environment.
-		final ExampleSetup setup = new ExampleSetup();
+		setup = new ExampleSetup();
 		messaging.addObject(setup.getGroup());
 		messaging.addObject(setup.getFiniteField());
 		messaging.addObject(setup);
 		messaging.addObject(new EuroAssetType());
+		
+		// Register state handlers.
+		messaging.registerStateHandler(Issuer.class, new LoggingStateHandler<Issuer>());
+		messaging.registerStateHandler(Bearer.class, new LoggingStateHandler<Bearer>());
+		messaging.registerStateHandler(IOweYou.class, new LoggingStateHandler<IOweYou>());
+		
+		messaging.registerStateHandler(AbstractBank.class, new StateHandler<AbstractBank>(){
+
+			@Override
+			public void enteredState(AbstractBank object, String enteredBy) {
+				synchronized(banks) {
+					banks.put(object.getIdentity(), object);
+				}
+			}
+
+			@Override
+			public void leftState(AbstractBank object) {
+				synchronized(banks) {
+					banks.remove(object.getIdentity());
+				}
+			}
+		});
+		
+		// Restore existing objects from storage
+		messaging.restoreFromObjectStorage();
+	}
+	
+	private class LoggingStateHandler<C> implements StateHandler<C> {
+
+		@Override
+		public void enteredState(C object, String enteredBy) {
+			if (object instanceof Identifiable) {
+				System.out.println("enteredState(" + ((Identifiable) object).getIdentity() + ": " + object.toString() + ", \"" + enteredBy + "\")");
+			} else {
+				System.out.println("enteredState(" + object.toString() + ", \"" + enteredBy + "\")");
+			}
+		}
+
+		@Override
+		public void leftState(C object) {
+			System.out.println("leftState(" + object.toString() + ")");
+		}
+	}
+	
+	public void mainInteractive(String[] args) throws IOException, InterruptedException {
+		setupNode(args.length > 1 ? args[1] : "interactive");
+		
+		BufferedReader in = new BufferedReader(new InputStreamReader(System.in, "UTF-8"));
+		for (String line = in.readLine(); line != null; line = in.readLine()) {
+			StringTokenizer st = new StringTokenizer(line);
+			if (st.hasMoreTokens()) {
+				String command = st.nextToken();
+				
+				if ("quit".equalsIgnoreCase(command)) {
+					messaging.shutdown();
+					return;
+				}
+				
+				if ("newbank".equalsIgnoreCase(command)) {
+					ExampleBank bank = new ExampleBank(setup);
+					messaging.addObject(bank, true);
+				}
+				
+				if ("newshop".equalsIgnoreCase(command)) {
+					if (st.hasMoreTokens()) {
+						String identity = st.nextToken();
+						ExampleShop shop = new ExampleShop(setup, identity);
+						messaging.addObject(shop);
+					}
+				}
+				
+				if ("newaccount".equalsIgnoreCase(command)) {
+					if (st.hasMoreTokens()) {
+						String bankIdentity = st.nextToken();
+						Bank bank = null;
+						synchronized(banks) {
+							bank = banks.get(bankIdentity);
+						}
+						if (bank != null) {
+							OpeningAccountHolder accountHolder = new OpeningAccountHolder(setup, bank);
+							messaging.addObject(accountHolder);
+						} else {
+							System.out.println("No such bank");
+						}
+					}
+				}
+				
+			}
+		}
+	}
+
+	/**
+	 * @param args
+	 * @throws InterruptedException
+	 * @throws IOException
+	 */
+	public void mainReal(String[] args) throws InterruptedException, IOException {
+		final String localId = "real1"; //UUID.randomUUID().toString();
+		setupNode("real1");
 
 		// Setup the bank.
 		final AbstractBankPrivate<Gq, Z, Long, ExampleSignatureHashFunction, ExamplePaymentHashFunction> bank = new ExampleBank(
@@ -265,25 +376,8 @@ public class Main {
 	 * @throws InterruptedException
 	 * @throws IOException
 	 */
-	public static void mainReal2(String[] args) throws InterruptedException, IOException {
-		final String localId = "real2"; //UUID.randomUUID().toString();
-		final JsonUtil jsonUtil = new JsonUtil(EuroAssetType.class, Z.class, Z.ZElement.class, Gq.class,
-				Gq.GqElement.class, ExampleSetup.class, ExampleBank.class, RemoteBank.class, BlindedIdentity.class,
-				IssuedWitnesses.class, ExampleAccountHolder.class, OpeningAccountHolder.class, ExampleShop.class,
-				ExampleCoinSignature.class, WithdrawingCoinOne.class, WithdrawingCoinTwo.class,
-				ExampleUnspentCoin.class, TransferringCoinOne.class, ReceivingCoin.class, ExampleSpentCoin.class,
-				BankWitnessesRequest.class, BankWitnessesResponse.class, BlindedIdentityRequest.class,
-				BlindedIdentityResponse.class, IssueCoinsRequest.class, IssueCoinsResponse.class,
-				ObtainCoinsMessage.class, TransferToShopMessage.class, CoinHashRequest.class, CoinHashResponse.class,
-				CoinTransferMessage.class);
-		final MessagingSystem messaging = new MessagingSystem(jsonUtil, localId);
-
-		// Setup the environment.
-		final ExampleSetup setup = new ExampleSetup();
-		messaging.addObject(setup.getGroup());
-		messaging.addObject(setup.getFiniteField());
-		messaging.addObject(setup);
-		messaging.addObject(new EuroAssetType());
+	public void mainReal2(String[] args) throws InterruptedException, IOException {
+		setupNode("real2");
 
 		// Setup the bank.
 //		final AbstractBankPrivate<Gq, Z, String, Long, ExampleSignatureHashFunction, ExamplePaymentHashFunction> bank = new ExampleBank(
